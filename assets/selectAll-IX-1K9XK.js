97bf274a61dc10931c1a6edf9aa72b0803792fc3
doczi-dominik <doczi.dominik0@gmail.com>
2024-02-21 15:15:17 +0100

    added build files

---
+++ assets/selectAll-IX-1K9XK.js
@@ -0,0 +1,2 @@
+import{S as t,D as n,E as o}from"./index-W2LHzQVk.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
+//# sourceMappingURL=selectAll-IX-1K9XK.js.map
