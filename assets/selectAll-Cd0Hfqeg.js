240f7b393dbea89d38ba9b569a1e4c2ddbdf6105
doczi-dominik <doczi.dominik0@gmail.com>
2024-02-21 18:58:21 +0100

    fixed filename, cleaned up html

---
+++ assets/selectAll-Cd0Hfqeg.js
@@ -0,0 +1,2 @@
+import{S as t,D as n,E as o}from"./index-BG9q160n.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
+//# sourceMappingURL=selectAll-Cd0Hfqeg.js.map
